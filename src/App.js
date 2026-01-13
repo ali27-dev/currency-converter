8009c89d2fe17522d7718b615e7d5751779386df
ali27-dev <khanali3229@gmail.com>
2026-01-13 21:59:36 +0500

    Finally the currency converter is working but some functionality is remaining,I will complete as soon as possible.

--- src/App.js
+++ src/App.js
@@ -10,46 +10,79 @@ export default function App() {
 }
 
 function CurrencyConverter() {
-  const [isText, setIsText] = useState(0);
-  const [isCurrency, setIsCurrency] = useState(0);
+  const [isAmount, setIsAmount] = useState(1);
+  const [fromCurrency, setFromCurrancy] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [toConverter, setToConverter] = useState("");
+  // const [isLoading, setLoading] = useState(false);
 
-  useEffect(function () {
-    async function fetchingCurrency() {
-      try {
-        const res = await fetch(
-          `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
-        );
+  useEffect(
+    function () {
+      async function fetchingCurrency() {
+        try {
+          // setLoading(true);
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${isAmount}&from=${fromCurrency}&to=${toCurrency}`
+          );
 
-        const data = await res.json();
-        console.log(data.rates);
-        setIsCurrency(data);
-      } catch (error) {
-        console.error("Not Fetching Currency!");
+          const data = await res.json();
+
+          setToConverter(data.rates[toCurrency]);
+          // setLoading(false);
+        } catch (error) {
+          console.error("Not Fetching Currency!");
+        }
       }
-    }
-    fetchingCurrency();
-  }, []);
+      if (fromCurrency === toCurrency) return setToConverter(isAmount);
+      // setLoading(false);
+      fetchingCurrency();
+    },
+    [fromCurrency, toCurrency, isAmount]
+  );
   return (
     <div className="">
-      <input
-        type="text"
-        placeholder="Enter Amount..."
-        onChange={(e) => setIsText(e.target.value)}
-      />
-      <select name="" id="">
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select name="" id="">
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
+      <>
+        <input
+          type="text"
+          placeholder="Enter Amount..."
+          value={isAmount}
+          onChange={(e) => Number(setIsAmount(e.target.value))}
+          // disabled={isLoading}
+        />
+        <select
+          name=""
+          id=""
+          value={fromCurrency}
+          onChange={(e) => setFromCurrancy(e.target.value)}
+          // disabled={isLoading}
+        >
+          <option value="EUR">EUR</option>
+          <option value="USD">USD</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
 
-      <h4>{isText * 385}</h4>
+        <select
+          name=""
+          id=""
+          value={toCurrency}
+          onChange={(e) => setToCurrency(e.target.value)}
+          // disabled={isLoading}
+        >
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      </>
+
+      <h4>
+        {toConverter} {toCurrency}
+      </h4>
     </div>
   );
 }
+
+// function Loding() {
+//   return <p className="loader">Data is fetching please wait....</p>;
+// }
