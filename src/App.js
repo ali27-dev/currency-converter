3dcffdda97026224f0a8624f7c92d00ce43d5e21
ali27-dev <khanali3229@gmail.com>
2026-01-13 15:43:14 +0500

    I'm handle the input field data but the other data are remining.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-function App() {
+import { useEffect, useState } from "react";
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <CurrencyConverter />
     </div>
   );
 }
 
-export default App;
+function CurrencyConverter() {
+  const [isText, setIsText] = useState(0);
+  const [isCurrency, setIsCurrency] = useState(0);
+
+  useEffect(function () {
+    async function fetchingCurrency() {
+      try {
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+        );
+
+        const data = await res.json();
+        console.log(data.rates);
+        setIsCurrency(data);
+      } catch (error) {
+        console.error("Not Fetching Currency!");
+      }
+    }
+    fetchingCurrency();
+  }, []);
+  return (
+    <div className="">
+      <input
+        type="text"
+        placeholder="Enter Amount..."
+        onChange={(e) => setIsText(e.target.value)}
+      />
+      <select name="" id="">
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select name="" id="">
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <h4>{isText * 385}</h4>
+    </div>
+  );
+}
